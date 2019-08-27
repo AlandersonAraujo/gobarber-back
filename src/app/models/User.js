b77604a3520efd9c5caea1efff37000480601689
AlandersonAraujo <alandersoncaraujo@gmail.com>
2019-08-26 22:07:40 -0300

    Migration 02

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -16,13 +16,21 @@ class User extends Model {
       }
     );
 
-    this.addHook('beforeSave', async (user) => {
+    this.addHook('beforeSave', async user => {
       if (user.password) {
         user.password_hash = await bcrypt.hash(user.password, 8);
       }
     });
 
     return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatar_id', as: 'avatar' });
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
   }
 }
 
