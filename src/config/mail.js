235915c602b395cae72d71ded3f69408fe01067a
AlandersonAraujo <alandersoncaraujo@gmail.com>
2019-08-29 20:45:44 -0300

    Envio de Email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'f26c8f9fb82af5',
+    pass: '11c4ebfba2d41b',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
